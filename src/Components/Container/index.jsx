3488a661b0379d7570c510b79f0f6ce81e6e6952
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-30 15:01:20 -0300

    implementando novas refatorações

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -13,6 +13,12 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  
+@media (max-width: 800px) {
+  flex-direction: column;
+
+}
 `
 
 export default () => {
