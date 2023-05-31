59f6af4a61c8e6f0fbe40836f2f2bb6a2e77c8cf
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-31 11:08:05 -0300

    implementando temas claro e escuro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { ThemeProvider } from "styled-components";
+import {temaClaro, temaEscuro} from "./Components/UI/temas";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
@@ -7,9 +8,11 @@ import { GlobalStyle } from "./Components/GlobalStyle";
 function App() {
   return (
     <>
-      <GlobalStyle/>
-      <Cabecalho />
-      <Container />
+      <ThemeProvider theme={temaEscuro}>
+        <GlobalStyle/>
+        <Cabecalho />
+        <Container />
+      </ThemeProvider>
     </>
   );
 }
